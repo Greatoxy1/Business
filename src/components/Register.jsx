f7ebd88e0729052a3cf232d35d7cf18c16fe6bb7
Greatoxy1 <156723421+Greatoxy1@users.noreply.github.com>
2026-05-17 22:52:07 +0200

    add send-message route

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -33,32 +33,66 @@ export default function Register() {
       alert("Registration failed");
     }
   };
+return (
+  <div>
+    <h2>Register</h2>
 
-  return (
-    <div>
-      <h2>Register</h2>
-      <input placeholder="Username" id="username"
-        name="username" autoComplete="username"
-        value={username}
-        onChange={e => setUsername(e.target.value)} />
-      <input type="password" id="password"
-        name="password" placeholder="Password" autoComplete="new-password"
-        value={password} onChange={e => setPassword(e.target.value)} />
-      <input type="text" id="country"
-        name="country" placeholder="Country" value={country}
-        onChange={e => setCountry(e.target.value)} />
-      <input type="text" id="city"
-        name="city" placeholder="City" value={city}
-        onChange={e => setCity(e.target.value)} />
-      <input type="text" id="town"
-        name="town" placeholder="Town" value={town}
-        onChange={e => setTown(e.target.value)} />
-
-      <form onSubmit={(e) => {
+    <form
+      onSubmit={(e) => {
         e.preventDefault();
         handleRegister();
-      }}></form>
-      <button type="submit">Register</button>
-    </div>
-  );
+      }}
+    >
+      <input
+        placeholder="Username"
+        id="username"
+        name="username"
+        autoComplete="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <input
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Password"
+        autoComplete="new-password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <input
+        type="text"
+        id="country"
+        name="country"
+        placeholder="Country"
+        value={country}
+        onChange={(e) => setCountry(e.target.value)}
+      />
+
+      <input
+        type="text"
+        id="city"
+        name="city"
+        placeholder="City"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+
+      <input
+        type="text"
+        id="town"
+        name="town"
+        placeholder="Town"
+        value={town}
+        onChange={(e) => setTown(e.target.value)}
+      />
+
+      <button type="submit">
+        Register
+      </button>
+    </form>
+  </div>
+);
 }
